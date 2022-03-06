278c756e6aa2dda21f20fdc01b0cb24d0a4c5c6a
Michael DoHo <michaeldoho@hotmail.com>
2022-03-06 14:16:27 -0500

    learned ab vue create.app/mounting app with vue. started writing notes on code, so i can explain to others better

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp ({ //we're putting all of our vue data/methods into app on line12 of index.html
+    data: function() {
+        return {
+            product: 'Socks', //now product socks is a data item
+            description: 'description: made with wool'
+        }
+    }
+})
